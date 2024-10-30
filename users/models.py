375c737d7e733e2efb389c0af2fd88cef5efa9ef
Jakob Cantrelle <131471949+JakobCantrelle@users.noreply.github.com>
2024-10-30 14:19:59 -0500

    Added user and passwords models and admin panel

--- users/models.py
+++ users/models.py
@@ -24,6 +24,11 @@ class CustomUser(AbstractUser):
 
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    is_paid = models.BooleanField(_("is paid"), default=False)
+    num_stored_passwords = models.IntegerField(_("number of stored passwords"), default=0)
+    paid_access_expires = models.DateTimeField(_("paid access expires"), null=True, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -40,3 +45,24 @@ class CustomUser(AbstractUser):
 
         """
         return self.email
+
+
+class Password(models.Model):
+    """A model for storing encrypted passwords associated with a user."""
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="passwords")
+    username = models.CharField(_("username"), max_length=255)
+    website = models.CharField(_("website"), max_length=255)
+    website_username = models.CharField(_("website username"), max_length=255)
+    is_password = models.BooleanField(_("is password"), default=True)
+    created = models.DateTimeField(_("created"), auto_now_add=True)
+    last_used = models.DateTimeField(_("last used"), null=True, blank=True)
+    password_encrypted = models.CharField(_("password encrypted"), max_length=255)
+
+    def __str__(self):
+        """Get the string representation of the object."""
+        return f"{self.website} - {self.website_username}"
+
+    class Meta:
+        verbose_name = _("Password")
+        verbose_name_plural = _("Passwords")
