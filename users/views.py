b367da874a1d24cc7d57251c052bbff56e17a882
GreenMachine414 <Tmikeandrus@gmail.com>
2024-11-05 17:34:50 -0600

    Finally got this working

--- users/views.py
+++ users/views.py
@@ -18,19 +18,19 @@ class SignUpView(CreateView):
     extra_context = {"title_text": "Sign Up", "button_text": "Register"}
 
 
-class CreateUserView(generic.CreateView):
-    """View to create user"""
+class CreateUserView(CreateView):
+    """View to create user."""
 
     model = CustomUser
-    fields = ["first_name"]
+    fields = ["username", "email"]  # Include 'email' in the fields
     success_url = reverse_lazy("users:user-list")
     template_name = "generic_create_update_form.html"
     extra_context = {"title_text": "Add User", "button_text": "Add"}
 
 
 class UserListView(generic.ListView):
-    """List of each users"""
+    """List of each users."""
 
     model = CustomUser
     queryset = CustomUser.objects.order_by("-date_joined")
-    template_name = "users/templates/generic_create_update_form.html"
+    template_name = "user_list.html"
